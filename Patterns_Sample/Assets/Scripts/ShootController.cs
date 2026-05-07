59d1195aebe048dba00b01cbdacdf7941beef4e7
CaroG1986 <carolina.garciao@upb.edu.co>
2026-05-06 20:38:10 -0500

    Inicio pool
    Interfaz inicial del pool

--- Patterns_Sample/Assets/Scripts/ShootController.cs
+++ Patterns_Sample/Assets/Scripts/ShootController.cs
@@ -1,6 +1,54 @@
+using System.Collections;
 using UnityEngine;
 
-public class ShootController
+public class ShootController : MonoBehaviour
 {
-    
+    [SerializeField]
+    private float bulletSpeed = 5f;
+
+    [SerializeField]
+    private float tripleShotDuration = 5f;
+
+    private IShoot currentShoot;
+
+    private void Start()
+    {
+        // Decorator inicial
+        currentShoot = new BasicShootDecorator(
+            null,
+            Player.Instance.BulletSpawnPoint,
+            bulletSpeed
+        );
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentShoot.Shoot();
+        }
+    }
+
+    public void ActivateTripleShot()
+    {
+        currentShoot = new TripleShootDecorator(
+            currentShoot,
+            this,
+            Player.Instance.BulletSpawnPoint,
+            bulletSpeed
+        );
+
+        StartCoroutine(TripleShotTimer());
+    }
+
+    private IEnumerator TripleShotTimer()
+    {
+        yield return new WaitForSeconds(tripleShotDuration);
+
+        currentShoot = new BasicShootDecorator(
+            null,
+            Player.Instance.BulletSpawnPoint,
+            bulletSpeed
+        );
+    }
 }
